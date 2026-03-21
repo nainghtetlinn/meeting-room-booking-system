1185c2b46063b76b27ef670b3ca654ec544622ed
nainghtetlinn <naing.work.it@gmail.com>
2026-03-21 23:14:11 +0630

    feat: update user create with optional role

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { UserRole } from '../entities/user.role';
 
 export class CreateUserDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name!: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  role?: UserRole;
 }

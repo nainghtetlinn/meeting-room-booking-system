71fb0585d561228b5e4620155af78f53c09f3e75
nainghtetlinn <naing.work.it@gmail.com>
2026-03-23 21:18:42 +0630

    feat: add cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const rawCors = process.env.CORS || '[]';
+  const cors = JSON.parse(rawCors) as string[];
+
+  app.enableCors({
+    origin: cors,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
